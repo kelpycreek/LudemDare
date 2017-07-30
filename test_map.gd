e257c500d942f71e253191198856e6b81a27250d
Forrest Keppler <kelpycreek@gmail.com>
2017-07-29 17:16:31 -0700

    updated scripts

--- test_map.gd
+++ test_map.gd
@@ -1,18 +1,37 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
 var grid
 var mech1
+var mech2
+var turn # whose turn is it
+var ai
 
 func _ready():
 	set_process_input(true)
-
+	set_process(true)
+	mech1 = get_node("Player/mech1")
+	mech2 = get_node("AI/mech2")
+	turn = 0
+	ai = get_node("AI")
+	grid = load("res://grid.gd").new(50)
+	var collision_map = get_node("map/TileMap").get_used_cells()
+	grid.load_collison_map(collision_map)
 	
-func _input(ev):
-	# Mouse in viewport coordinates
 
+func _input(ev):
+	if turn == 1:
+		return # player cant take actions off turn
 	if (ev.type==InputEvent.MOUSE_BUTTON):
-		get_node("mech1").move(ev.pos)
+		var square = grid.get_cell(ev.pos)
+		if grid.is_okay(square):
+			mech1.move(grid.get_coord(square))
+	if (ev.type==InputEvent.KEY):
+		mech1.attack("shoot", mech2)
+		pass_turn()
+		
+func pass_turn():
+	turn = 1
+	ai.take_turn()
+	turn = 0
+	
